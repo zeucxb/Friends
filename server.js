4db8ed2d0e85448752528a61e12352a5c56bf17a
anzileiro <anderson.anzileiro@gmail.com>
2016-07-21 00:37:14 -0300

    added routes and controllers

--- server.js
+++ server.js
@@ -2,17 +2,20 @@
 
 const Restify           = require('restify')
 ,     Configuration     = require('./lib/utils/configuration.js')
+,     UserRoutes        = require('./lib/routes/user.js')
 
 let Server = Restify.createServer(Configuration.server.info)
 
+//middleware
 Server.use(Restify.gzipResponse())
 Server.use(Restify.bodyParser())
 
-Server.get('/try/', (req, res, next) => {
-    res.send('me test')
-    return next()
-});
+//routes
+UserRoutes.forEach((route) => {
+    Server[route.method](route.path, route.handler)
+})
 
+//startup
 Server.listen(Configuration.server.port, () => {
     console.log(`${ Server.name } listening at ${ Server.url }`)
 })
